442048d1dd2831ee0b5448ee048a159e498ce3f7
Yohan Beugin <yohan@beugin.org>
2024-07-04 18:45:19 -0500

    Post Analysis RWS & Attestation

---
+++ analysis.sh
@@ -0,0 +1,73 @@
+#!/bin/bash
+
+last_analysis_stop_time=last_analysis_stop_time.txt
+raw_results_dir=results
+analysis_dir=analysis
+
+attestation_known_apis_suffix=attestation_known_apis.tsv
+attestation_known_origins_suffix=attestation_known_origins.json
+rws_known_origins_suffix=rws_known_origins.json
+
+rm -r $analysis_dir && mkdir -p $analysis_dir
+
+#get file storing filename of where last analysis stopped
+if [[ -z "$S3_ANALYSIS_BUCKET" ]];then
+    echo "S3_ANALYSIS_BUCKET undefined, assuming local run: not grabbing $last_analysis_stop_time from s3"
+else
+    aws s3 cp s3://$S3_ANALYSIS_BUCKET/$last_analysis_stop_time $last_analysis_stop_time
+fi
+
+#parse timestamp and convert format for date to understand
+last_timestamp=$(cat $last_analysis_stop_time | sed 's/-/ /g' | sed 's/_/-/g' | sed 's/-/:/g3')
+stop_time=$(date --date "$last_timestamp" +'%s')
+
+
+if [[ -z "$S3_DATA_BUCKET" ]];then
+    echo "S3_DATA_BUCKET undefined, assuming local run: not grabbing crawl results from s3"
+else
+    #get files from S3 bucket
+    aws s3 cp s3://$S3_DATA_BUCKET/$attestation_known_apis_suffix ${analysis_dir}/${attestation_known_apis_suffix}
+    aws s3 cp s3://$S3_DATA_BUCKET/$attestation_known_origins_suffix ${analysis_dir}/${attestation_known_origins_suffix}
+    aws s3 cp s3://$S3_DATA_BUCKET/$rws_known_origins_suffix ${analysis_dir}/${rws_known_origins_suffix}
+
+    #obtain filenames of objects in s3 buckets
+    s3_filenames=$(aws s3 ls s3://$S3_DATA_BUCKET | awk '{print $4}' | sed -n s/'\([0-9_-]*\).tar.zst/\1/p')
+    #loop through filanmes to grab only the new ones since last analysis
+    while IFS= read -r filename
+    do
+        #parse timestamp and convert format for date to understand
+        filename_timestamp=$(echo $filename | sed 's/-/ /g' | sed 's/_/-/g' | sed 's/-/:/g3')
+        filename_time=$(date --date "$filename_timestamp" +'%s')
+
+        if [ $filename_time -gt $stop_time ]
+        then
+            aws s3 cp s3://$S3_DATA_BUCKET/$filename_timestamp.tar.zst - | tar --zstd -xf -C $raw_results_dir/
+        fi
+    done <<< "$s3_filenames"
+fi
+
+#for loop on results folder, check again if time is greater, if so, run
+#analysis, and update times
+for crawl_filename in $raw_results_dir/*
+do
+    crawl_filetime=$(echo "${crawl_filename##*/}") #remove folder prefix
+    #parse timestamp and convert format for date to understand
+    crawl_timestamp=$(echo $crawl_filetime | sed 's/-/ /g' | sed 's/_/-/g' | sed 's/-/:/g3')
+    crawl_time=$(date --date "$crawl_timestamp" +'%s')
+
+    if [ $crawl_time -gt $stop_time ]
+    then
+        ./post_crawl_analysis.sh $crawl_filetime
+         #update stop timestamp, keep going
+        stop_time=$crawl_time
+        echo $crawl_filetime > $last_analysis_stop_time
+    fi
+done
+
+#update file storing filename of where last analysis stopped
+if [[ -z "$S3_ANALYSIS_BUCKET" ]];then
+    echo "S3_ANALYSIS_BUCKET undefined, assuming local run: not updating $last_analysis_stop_time to s3"
+else
+    aws s3 cp $last_analysis_stop_time s3://$S3_ANALYSIS_BUCKET/$last_analysis_stop_time
+fi
+
--- post_crawl_analysis.sh
+++ post_crawl_analysis.sh
@@ -1,86 +1,53 @@
 #!/bin/bash
 
-results_dir=results
-if [ "$#" -ne 1 ] || ! [ -d "${results_dir}/$1" ]; then
+raw_results_dir=results
+analysis_dir=analysis
+
+if [ "$#" -ne 1 ] || ! [ -d "${raw_results_dir}/$1" ]; then
   echo "Usage: $0 TIMESTAMP_CRAWL_DIR" >&2
   exit 1
 fi
 
 timestamp=$1
-results_crawl_dir=${results_dir}/${timestamp}
 
-# ATTESTATION
-attestation_dir=${results_crawl_dir}/attestation
-attestation_origins=${results_crawl_dir}/attestation_origins.txt
-attestation_apis_suffix=attestation_apis.tsv
-attestation_apis=${results_crawl_dir}/${attestation_apis_suffix}
+echo "Start analysis for $timestamp"
 
-attestation_known_origins=${results_dir}/attestation_known_origins.json
-attestation_known_origins_tmp=${results_dir}/attestation_known_origins.json.tmp
-attestation_known_apis=${results_dir}/attestation_known_apis.tsv
+raw_results_crawl_dir=${raw_results_dir}/${timestamp}
+analysis_crawl_dir=${analysis_dir}/${timestamp}
 
-if [ -f $attestation_apis ]; then
-    rm $attestation_apis
-fi
-
-if [ -f $attestation_origins ]; then
-    rm $attestation_origins
-fi
-echo -e "enrollment_site\tplatform\tapi" > $attestation_apis
-for entry in ${attestation_dir}/*
-do
-    #extract origins of enrollment_site
-    jq -r '.privacy_sandbox_api_attestations[0] | .enrollment_site' $entry >> $attestation_origins
-    #extract enrollment_site platform and apis from attestation files (only latest entry in attestation file)
-    jq -r '(.privacy_sandbox_api_attestations[0] | [ .enrollment_site ]  + (.platform_attestations[] | [.platform] + (.attestations | objects | keys[] as $k | [$k]))) | @tsv' $entry >> $attestation_apis
-
-done
-# keep unique apparitions
-sort -u $attestation_origins -o $attestation_origins
-sort -u $attestation_apis -o $attestation_apis
-
-#update json of known origins for next crawl/public
-while read -r LINE
-do
-    #jq update timestamp or insert into array if not present (assumption: post
-    #analysis script is run in sequential order, i.e., no check if timestamp
-    #higher than current one saved)
-    jq -r --arg ORIGIN "$LINE" --arg TIMESTAMP "$timestamp" '.known_origins |=  (map(.origin) | index($ORIGIN)) as $idx | if $idx then .[$idx]["latest_crawled"] = $TIMESTAMP else . + [{"origin": $ORIGIN, "latest_crawled": $TIMESTAMP}] end' $attestation_known_origins > $attestation_known_origins_tmp
-    rm $attestation_known_origins
-    mv $attestation_known_origins_tmp $attestation_known_origins
-done < $attestation_origins
-
-
-#update list of apis to release publicly
-if [ -f $attestation_known_apis ]; then
-    rm $attestation_known_apis
-fi
-echo -e "enrollment_site\tplatform\tapi\ttimestamp" > $attestation_known_apis
-#parse know origins for attestation, iterate through
-jq -r '.known_origins[] | [.origin] + [.latest_crawled] | @tsv' $attestation_known_origins | while IFS=$'\t' read domain date; do
-
-    if [ -d "${results_dir}/$date" ]; then
-        cat ${results_dir}/$date/${attestation_apis_suffix} | grep $domain | while IFS=$'\t' read d p a; do
-            echo -e "$d\t$p\t$a\t$date" >>$attestation_known_apis
-        done
-    fi
-done
-
-# keep unique apparitions
-sort -u $attestation_known_apis -o $attestation_known_apis
-
+mkdir -p $raw_results_crawl_dir $analysis_crawl_dir
 
 #RWS
-rws_dir=${results_crawl_dir}/rws
+rws_dir=${raw_results_crawl_dir}/rws
 
-rws_primary=${results_crawl_dir}/rws_primary.txt
-rws_associatedSites=${results_crawl_dir}/rws_associatedSites.txt
-rws_serviceSites=${results_crawl_dir}/rws_serviceSites.txt
-rws_ccTLDs=${results_crawl_dir}/rws_ccTLDs.txt
+rws_known_origins_suffix=rws_known_origins.json
 
-rws_known_origins=${results_dir}/rws_known_origins.json
-rws_known_origins_tmp=${results_dir}/rws_known_origins.json.tmp
+rws_known_origins=${analysis_dir}/${rws_known_origins_suffix}
+rws_known_origins_tmp=${analysis_dir}/${rws_known_origins_suffix}.tmp
 
+rws_primary=${analysis_crawl_dir}/rws_primary.txt
+rws_associatedSites=${analysis_crawl_dir}/rws_associatedSites.txt
+rws_serviceSites=${analysis_crawl_dir}/rws_serviceSites.txt
+rws_ccTLDs=${analysis_crawl_dir}/rws_ccTLDs.txt
+
+# ATTESTATION
+attestation_dir=${raw_results_crawl_dir}/attestation
+
+attestation_crawl_apis_suffix=attestation_crawl_apis.tsv
+attestation_known_apis_suffix=attestation_known_apis.tsv
+attestation_known_origins_suffix=attestation_known_origins.json
+
+attestation_known_origins=${analysis_dir}/${attestation_known_origins_suffix}
+attestation_known_origins_tmp=${analysis_dir}/${attestation_known_origins_suffix}.tmp
+attestation_known_apis=${analysis_dir}/${attestation_known_apis_suffix}
+
+attestation_crawl_apis=${analysis_crawl_dir}/${attestation_crawl_apis_suffix}
+attestation_crawl_origins=${analysis_crawl_dir}/attestation_crawl_origins.txt
+
+##############
+## RWS
+##############
+#extract origins from RWS files for current crawl
 if [ -f $rws_primary ]; then
     rm $rws_primary
 fi
@@ -93,7 +60,6 @@ fi
 if [ -f $rws_ccTLDs ]; then
     rm $rws_ccTLDs
 fi
-#extract origins from RWS files
 for entry in ${rws_dir}/*
 do
     jq -r '.primary' $entry >> $rws_primary
@@ -101,28 +67,108 @@ do
     jq -r 'select(.serviceSites != null) | .serviceSites[]' $entry >> $rws_serviceSites
     jq -r 'select(.ccTLDs != null) | .ccTLDs | objects | .[] | .[]' $entry >> $rws_ccTLDs
 done
-#keep unique apparitions
+#keep unique apparitions only
 sort -u $rws_primary -o $rws_primary
 sort -u $rws_associatedSites -o $rws_associatedSites
 sort -u $rws_serviceSites -o $rws_serviceSites
 sort -u $rws_ccTLDs -o $rws_ccTLDs
 
+# update general known origins file
+if [ ! -f $rws_known_origins ]; then
+    echo -e '{"known_origins": []}' > $rws_known_origins
+fi
 rws_files=($rws_primary $rws_associatedSites $rws_serviceSites $rws_ccTLDs)
 for file in "${rws_files[@]}"; do
     while read -r LINE
     do
-        #jq update timestamp or insert into array if not present
+        #jq update timestamp or insert into array if not present (assumption: post
+        #analysis script is run in sequential order, i.e., no check if timestamp
+        #higher than current one saved)
         jq -r --arg ORIGIN "$LINE" --arg TIMESTAMP "$timestamp" '.known_origins |=  (map(.origin) | index($ORIGIN)) as $idx | if $idx then .[$idx]["latest_crawled"] = $TIMESTAMP else . + [{"origin": $ORIGIN, "latest_crawled": $TIMESTAMP}] end' $rws_known_origins > $rws_known_origins_tmp
         rm $rws_known_origins
         mv $rws_known_origins_tmp $rws_known_origins
     done < $file
 done
 
+##############
+## ATTESTATION
+##############
+# extract attestation origins and apis for current crawl
+if [ -f $attestation_crawl_apis ]; then
+    rm $attestation_crawl_apis
+fi
+if [ -f $attestation_crawl_origins ]; then
+    rm $attestation_crawl_origins
+fi
+echo -e "enrollment_site\tplatform\tapi" > $attestation_crawl_apis
+for entry in ${attestation_dir}/*
+do
+    #extract origins of enrollment_site
+    jq -r '.privacy_sandbox_api_attestations[0] | .enrollment_site' $entry >> $attestation_crawl_origins
+    #extract enrollment_site platform and apis from attestation files (only latest entry in attestation file)
+    jq -r '(.privacy_sandbox_api_attestations[0] | [ .enrollment_site ]  + (.platform_attestations[] | [.platform] + (.attestations | objects | keys[] as $k | [$k]))) | @tsv' $entry >> $attestation_crawl_apis
+
+done
+# keep unique apparitions only
+sort -u $attestation_crawl_origins -o $attestation_crawl_origins
+sort -u $attestation_crawl_apis -o $attestation_crawl_apis
+
+# update general known origins file and list of apis
+if [ ! -f $attestation_known_origins ]; then
+    echo -e '{"known_origins": []}' > $attestation_known_origins
+fi
+if [ ! -f $attestation_known_apis ]; then
+    echo -e "enrollment_site\tplatform\tapi\ttimestamp" > $attestation_known_apis
+fi
+while read -r LINE
+do
+    #jq update timestamp or insert into array if not present (assumption: post
+    #analysis script is run in sequential order, i.e., no check if timestamp
+    #higher than current one saved)
+    jq -r --arg ORIGIN "$LINE" --arg TIMESTAMP "$timestamp" '.known_origins |=  (map(.origin) | index($ORIGIN)) as $idx | if $idx then .[$idx]["latest_crawled"] = $TIMESTAMP else . + [{"origin": $ORIGIN, "latest_crawled": $TIMESTAMP}] end' $attestation_known_origins > $attestation_known_origins_tmp
+    rm $attestation_known_origins
+    mv $attestation_known_origins_tmp $attestation_known_origins
+
+    #update api list, by removing if match domain to update with latest result
+    sed -i "\,$LINE,d" $attestation_known_apis
+    cat $attestation_crawl_apis | grep $LINE | while IFS=$'\t' read domain platform api; do
+        echo -e "$domain\t$platform\t$api\t$timestamp" >>$attestation_known_apis
+    done
+
+done < $attestation_crawl_origins
+# keep unique apparitions
+sort -u $attestation_known_apis -o $attestation_known_apis
 
 
-# rws_github_origins=${results_crawl_dir}/rws_github_origins.txt
-# rws_diff_on_github=${results_crawl_dir}/rws_diff_on_github.txt
-# rws_diff_not_on_github=${results_crawl_dir}/rws_diff_not_on_github.txt
+##############
+# S3 upload
+##############
 
-# diff $rws_origins $rws_github_origins | grep ">" | sed 's/^> //g' > $rws_diff_on_github
-# diff $rws_origins $rws_github_origins | grep "<" | sed 's/^< //g' > $rws_diff_not_on_github
+if [[ -z "$S3_DATA_BUCKET" ]];then
+    echo "S3_DATA_BUCKET undefined, not uploading results"
+else
+    #upload known origins for attestation and RWS + api list
+    aws s3 cp $attestation_known_origins s3://$S3_DATA_BUCKET/$attestation_known_origins_suffix
+    aws s3 cp $rws_known_origins s3://$S3_DATA_BUCKET/$rws_known_origins_suffix
+    aws s3 cp $attestation_known_apis s3://$S3_DATA_BUCKET/$attestation_known_apis_suffix
+fi
+
+if [[ -z "$S3_PUBLIC_BUCKET" ]];then
+    echo "S3_PUBLIC_BUCKET undefined, not uploading results"
+else
+    #upload known origins for attestation and RWS + api list
+    aws s3 cp $attestation_known_origins s3://$S3_PUBLIC_BUCKET/well-known-crawler/$attestation_known_origins_suffix
+    aws s3 cp $rws_known_origins s3://$S3_PUBLIC_BUCKET/well-known-crawler/$rws_known_origins_suffix
+    aws s3 cp $attestation_known_apis s3://$S3_PUBLIC_BUCKET/well-known-crawler/$attestation_known_apis_suffix
+fi
+
+if [[ -z "$S3_ANALYSIS_BUCKET" ]];then
+    echo "S3_ANALYSIS_BUCKET undefined, not uploading results"
+else
+    #upload analysis folder to s3
+    cd $analysis_dir
+    tar --zstd -c $timestamp | aws s3 cp - s3://$S3_ANALYSIS_BUCKET/$timestamp.tar.zst
+    cd ..
+fi
+
+echo "Analysis Finished for $timestamp"
